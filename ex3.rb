85c870cc760b594b9fd99e6fc3458658ef1eff3d
Michael Jackson <michael.lenard.jackson@gmail.com>
2016-07-03 00:41:16 -0500

    Exercises 1 - 5
    Exercise 1: A Good First Program
    Exercise 2: Comments and Pound Characters
    Exercise 3: Numbers and Math
    Exercise 4: Variables And Names
    Exercise 5: More Variables and Printing

---
+++ ex3.rb
@@ -0,0 +1,27 @@
+puts "I will now count my chickens:"
+# Pound sign inside of "quotes" work similar to a function
+
+# Math is performed in traditional PEMDAS order of operations
+
+puts "Hens #{25 + 60 / 6}"
+puts "Roosters #{100  - 25 * 3 % 4}"
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7"
+# Compares values in true false
+puts 3 + 2 < 5 - 7
+#Prints out sum
+puts "What is 3 + 2 ? #{3 + 2}"
+puts "What is 5 - 7 ? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+# Greater than, less than, and equal Output True/False values
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Michael L. Jackson'
+my_age = 28 # As of July 2, 2016
+my_height = 76 # Inches
+my_weight = 290 # Lbs
+my_eyes = 'Browm'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's kind of heavy." #tee hee :)
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
